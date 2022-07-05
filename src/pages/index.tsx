a82d27a02c86aa2bd26575d3e2dcfc0b9e742221
Luis Souza <luis.souza@mktsystems.com>
2022-07-05 20:39:50 -0300

    feat: home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,22 @@
-import type { NextPage } from 'next'
+import type { NextPage } from 'next';
+
+import styles from './home.module.scss';
 
 const Home: NextPage = () => {
   return (
-    <div>
-      <p>Ig.news</p>
-    </div>
+    <main className={styles.contentContainer}>
+      <section className={styles.hero}>
+        <span>👏 Hey, welcome</span>
+        <h1>New about the <span>React</span> world.</h1>
+
+        <p>
+          Get access to all the publications <br />
+          <span>for $9.90 month</span>
+        </p>
+      </section>
+
+      <img src="/images/avatar.svg" alt="Girl Coding" />
+    </main>
   )
 }
 
