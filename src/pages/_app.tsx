f67cfb3bb13f1709795966456dfa63455cdccdfe
Luis Souza <luis.souza@mktsystems.com>
2022-07-13 22:04:12 -0300

    feat: add next-auth

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,14 +1,16 @@
 import type { AppProps } from 'next/app';
+import { SessionProvider } from 'next-auth/react';
+
 import { Header } from '../components/Header';
 
 import '../styles/global.scss';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <>
+    <SessionProvider>
       <Header />
       <Component {...pageProps} />
-    </>
+    </SessionProvider>
   )
 }
 
