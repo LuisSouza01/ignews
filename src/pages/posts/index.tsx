92d4ea8b15161a4097043aba7a14939ebdc19d42
Luis Souza <luis.souza@mktsystems.com>
2022-07-19 23:15:17 -0300

    feat: add post page

--- src/pages/posts/index.tsx
+++ src/pages/posts/index.tsx
@@ -1,7 +1,6 @@
-import { GetStaticProps } from 'next';
-import * as Prismic from '@prismicio/client';
 import Head from 'next/head';
-
+import { GetStaticProps } from 'next';
+import Link from 'next/link';
 import { RichText } from 'prismic-dom';
 
 import { getPrismicClient } from '../../services/prismic';
@@ -23,20 +22,19 @@ export default function Posts({ posts }: PostsProps) {
   return (
     <>
       <Head>
-        <title>Post | ig.news</title>
+        <title>Posts | ig.news</title>
       </Head>
 
       <main className={styles.container}>
         <div className={styles.posts}>
           {posts?.map((post) => (
-            <a
-              key={post.slug}
-              href={`/posts/${post.slug}`}
-            >
-              <time>{post.updatedAt}</time>
-              <strong>{post.title}</strong>
-              <p>{post.content}</p>
-            </a>
+            <Link key={post.slug} href={`/posts/${post.slug}`}>
+              <a>
+                <time>{post.updatedAt}</time>
+                <strong>{post.title}</strong>
+                <p>{post.content}</p>
+              </a>
+            </Link>            
           ))}
         </div>
       </main>
